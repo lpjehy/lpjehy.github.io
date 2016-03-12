4cf556eacfb4827a8523637be3722d5b97353bc8
Jehy <lpjehy@foxmail.com>
2016-03-12 20:36:38 +0800

    no message

--- js/common.js
+++ js/common.js
@@ -2,6 +2,6 @@ var GetQueryString = function(name) {
         var reg = new RegExp("(^|&)" + name + "=([^&]*)(&|$)");
         var r = window.location.search.substr(1).match(reg);
         if (r != null)
-            return unescape(r[2]);
+            return r[2];
         return null;
     }
